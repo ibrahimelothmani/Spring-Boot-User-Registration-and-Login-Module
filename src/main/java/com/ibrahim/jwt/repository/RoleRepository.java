41a9977cbba3cb8e64b2eab36c0e9659382eabcf
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-25 07:19:37 -1200

    Create Repositories

---
+++ src/main/java/com/ibrahim/jwt/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.ibrahim.jwt.repository;
+
+import com.ibrahim.jwt.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/ibrahim/jwt/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ibrahim.jwt.repository;
+
+import com.ibrahim.jwt.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
