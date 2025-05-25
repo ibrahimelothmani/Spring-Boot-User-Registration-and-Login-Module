49b275eff061d1cecef7f035b83d0ea1051f934e
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-25 07:00:08 -1200

    Spring Boot User Registration and Login Module using Spring Boot--Spring Security--Hibernate, and Thymeleaf

---
+++ src/test/java/com/ibrahim/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
